c7a7cae195a733aafbf8665a82b2bd83e329a8f2
prakhar katiyar <prkhrkat@gmail.com>
2024-08-05 17:08:43 +0530

    change

--- greet.go
+++ greet.go
@@ -1,8 +1,12 @@
 package commonlib
 
-func Greet(name string, formal bool) string {
-    if formal {
-        return "Hello, " + name + "."
-    }
-    return "Hi, " + name + "!"
+//func Greet(name string, formal bool) string {
+//    if formal {
+//        return "Hello, " + name + "."
+//    }
+//    return "Hi, " + name + "!"
+//}
+
+func Greet(name string) string {
+	return "Hello, " + name + "!"
 }
